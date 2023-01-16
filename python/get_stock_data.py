c2657ffaa0cdbad029346032b1d642b537a955f8
= <=>
2023-01-16 21:56:08 +0000

    Experimented to find the best number of estimators

--- python/get_stock_data.py
+++ python/get_stock_data.py
@@ -5,6 +5,9 @@ import pandas as pd
 import matplotlib as plt
 import numpy as np
 from typing import *
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 def update_sp500_tickers(path: str, folder: str):
@@ -54,6 +57,23 @@ def get_all_stocks_histories(path: str):
     except FileNotFoundError:
         print("[Error] Path '{}' doesn't exist!")
 
+def find_best_estimators(x_train, y_train, x_test, y_test):
+    estimators = [5, 10, 25, 30, 50, 60, 75, 100]
+    accuracies = []
+    for est in estimators:
+        clf1 = RandomForestClassifier(n_estimators=est, random_state=42)  # random_state=42
+
+        # Fit the classifier to the training data
+        clf1.fit(x_train, y_train)
+
+        # Make predictions on the test data
+        predictions = clf1.predict(x_test)
+        accuracies.append(accuracy_score(y_test, predictions))
+
+    best = max(accuracies)
+    _index = accuracies.index(best)
+    return accuracies[_index], estimators[_index]
+
 
 print("Starting...")
 folder = "data"
@@ -63,11 +83,13 @@ path = os.path.join(os.pardir, folder + "/" + filename)
 # Un-comment this code to download latest training data
 # update_sp500_tickers(path, folder)
 
+
 sp500_tickers = get_sp500_tickers(path)
 data = pd.read_csv(os.pardir + "/data/individual_stock_data/XOM.csv")
-data['gold_std'] = data['Open'].shift(-1)
-true_data = data[['Open', 'gold_std']]
-true_data = true_data[:-1]
+# data['gold_std'] = data['Open'].shift(-1)
+# true_data = data[['Open', 'gold_std']]
+true_data = data[['Open']]
+
 
 # Assume that your DataFrame is called 'df'
 
@@ -88,19 +110,49 @@ avg_gain = gain.rolling(window=14).mean()
 avg_loss = loss.rolling(window=14).mean()
 rs = avg_gain / avg_loss
 true_data['rsi'] = 100 - (100 / (1 + rs))
-print(true_data.columns.values)
 true_data = true_data[20:]
+true_data['label'] = str('NaN')
+np_array_data = np.array(true_data)
+for i in range(len(np_array_data) - 1):
+    if np_array_data[i][0] <= np_array_data[i+1][0]:
+        np_array_data[i][-1] = "up"
+    else:
+        np_array_data[i][-1] = "down"
 
-# true_data = true_data[-2000:]
+np_array_data = np_array_data[:-1]
+labels = np_array_data[:, [7]]
+features = np_array_data[:, range(0, 7)]
+print(features)
+print(labels)
 
-# print(true_data[:-1])
-features = np.array(true_data[['Open', 'ma10', 'ma20', 'std20', 'upper_band', 'lower_band']])
-labels = np.array(true_data['gold_std'])
+# print(np_array_data[:-1, [0, 7]])
+# df = pd.DataFrame({"Open": np_array_data[:, [0]],
+#                    "ma10": np_array_data[:, [1]],
+#                    "ma20": np_array_data[:, [2]],
+#                    "std20": np_array_data[:, [3]],
+#                    "upper_band": np_array_data[:, [4]],
+#                    "lower_band": np_array_data[:, [5]],
+#                    "rsi": np_array_data[:, [6]],
+#                    "label": np_array_data[:, [7]]
+#                    })
+#
 
-_tickers = get_sp500_tickers(path)
-downlaod_stock_histories(os.pardir + "/data/", sp500_tickers[:10])
-histories = get_all_stocks_histories(os.pardir + "/data/")
-print(histories)
-# histories = get_all_stocks_histories(sp500_tickers[:10])
+x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
+#Create an instance of the classifier
+# clf1 = RandomForestClassifier(n_estimators=100) #random_state=42
+#
+# #Fit the classifier to the training data
+# clf1.fit(x_train, y_train)
+#
+# #Make predictions on the test data
+# predictions = clf1.predict(x_test)
+#
+# #Evaluate the performance of the classifier
+# print("Random Forest Accuracy:", accuracy_score(y_test, predictions))
+# print("Random State: ", clf1.random_state)
+
+accuracy, estimators = find_best_estimators(x_train, y_train, x_test, y_test)
+print("Best accuracy: ", accuracy)
+print("Best estimators: ", estimators)
 
